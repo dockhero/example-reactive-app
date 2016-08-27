f45da6882e51a2762a9ceeab1f2d543ab00ada6a
Vladimir Yartsev <vovayartsev@gmail.com>
2016-08-27 18:56:50 +0400

    WIP

--- config/initializers/nobrainer.rb
+++ config/initializers/nobrainer.rb
@@ -1,3 +1,4 @@
 NoBrainer.configure do |config|
-  config.rethinkdb_urls = ["rethinkdb://#{ENV.fetch('DOCKHERO_HOST')}:28015/logsfeed"]
+  config.rethinkdb_urls = ["rethinkdb://admin:#{ENV.fetch('RETHINKDB_PASSWORD')}@#{ENV.fetch('DOCKHERO_HOST')}:28015/logsfeed"]
+  config.warn_on_active_record = false
 end
--- livestatus/server.js
+++ livestatus/server.js
@@ -5,13 +5,62 @@ var route = require("koa-route");
 var r = require("rethinkdb");
 var bluebird = require("bluebird");
 var rethinkdbStream = require('rethinkdb-stream');
-var sse = require('koa-sse');
 var Readable = require('stream').Readable;
 var map = require('map-stream');
 var cors = require('koa-cors');
+var sse = require("./sse.js");
 
-// http://stackoverflow.com/questions/8313628/node-js-request-how-to-emitter-setmaxlisteners
-require('events').EventEmitter.prototype._maxListeners = 30;
+
+
+app.use(createConnection);
+
+app.use(cors({origin: true}));
+
+app.use(route.get("/operations/:id", function *(id) {
+  // otherwise node will automatically close this connection in 2 minutes
+  this.req.setTimeout(Number.MAX_VALUE);
+
+  this.type = 'text/event-stream; charset=utf-8';
+  this.set('Cache-Control', 'no-cache');
+  this.set('Connection', 'keep-alive');
+
+  // fetching data
+
+  const cursor = yield r.table("operations")
+                        .get(id)
+                        .changes({include_initial: true})
+                        .run(this._rdbConn);
+
+  var stream = rethinkdbStream(cursor)
+                // .pipe(extractNewVal)
+
+  var body = this.body = sse();
+
+  stream.pipe(body);
+
+  // if the connection closes or errors,
+  // we stop the SSE.
+  var socket = this.socket;
+  socket.on('error', close);
+  socket.on('close', close);
+
+  function close() {
+    stream.unpipe(body);
+    stream.close();
+    socket.removeListener('error', close);
+    socket.removeListener('close', close);
+  }
+}));
+
+
+app.listen(8090);
+console.log("Listening on http://localhost:8090/");
+
+// helpers
+
+const extractNewVal = map((data, callback) => {
+  callback(null, data["new_val"]);
+})
 
 const connectOptions = {
   host: process.env.RETHINKDB_HOST || 'localhost',
@@ -19,22 +68,22 @@ const connectOptions = {
   db: process.env.RETHINKDB_DATABASE || 'logsfeed'
 };
 
-const extractNewVal = map((data, callback) => {
-  callback(null, data["new_val"]);
-})
+function* createConnection(next) {
+    try{
+        var conn = yield r.connect(connectOptions);
 
-bluebird.coroutine(function*() {
-  console.log("Connecting to RethinkDB...");
-  const conn = yield r.connect(connectOptions);
+        // Save the connection in the current context (will be passed to the next middleware)
+        this._rdbConn = conn;
 
-  app.use(cors());
-  app.use(sse());
+        this.socket.on('close', () => {
+          console.log("Closing connection");
+          this._rdbConn.close()
+        })
 
-  app.use(route.get("/operations/:id", function *(id) {
-    const cursor = yield r.table("operations").get(id).changes({include_initial: true}).run(conn);
-    this.sse(rethinkdbStream(cursor).pipe(extractNewVal));
-  }));
-
-  app.listen(8090);
-  console.log("Listening on http://localhost:8090/");
-})();
+        yield next;
+    }
+    catch(err) {
+        this.status = 500;
+        this.body = err.message || http.STATUS_CODES[this.status];
+    }
+}
